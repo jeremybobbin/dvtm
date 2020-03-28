2d1a7d8dd530b4dc9299470863939eb649b11b7a
Jeremy Bobbin <jer@jer.cx>
2020-03-28 12:06:30 -0700

    hide border when only one client is visible

--- dvtm.c
+++ dvtm.c
@@ -388,7 +388,7 @@ drawbar(void) {
 
 static int
 show_border(void) {
-	return (bar.pos != BAR_OFF) || (clients && clients->next);
+	return (bar.pos != BAR_OFF) || (clients && nextvisible(clients->next));
 }
 
 static void
